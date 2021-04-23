12d59671771db42c8c4fb3f6863e7e00d8b50ba8
Luiz Paduan <luiz.paduanit@gmail.com>
2021-04-22 23:55:44 -0300

    implementação incial planos

---
+++ app/Models/Plano.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plano extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nome',
+        'url',
+        'preco',
+        'descricao',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PlanoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('admin/planos', [PlanoController::class, 'index'])->name('planos.index');
 
 Route::get('/', function () {
     return view('welcome');

f58e01c7be92f2080bc03c5869bde993ea2f37c3
Luiz Paduan <luiz.paduanit@gmail.com>
2021-04-29 23:15:18 -0300

    Reorganização das rotas

--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('admin/planos/novo', [PlanoController::class, 'create'])->name('planos.create');
-Route::put('admin/planos/{url}', [PlanoController::class, 'update'])->name('planos.update');
-Route::get('admin/planos/{url}/editar', [PlanoController::class, 'edit'])->name('planos.edit');
-Route::any('admin/planos/filtrar', [PlanoController::class, 'search'])->name('planos.search');
-Route::delete('admin/planos/{url}', [PlanoController::class, 'destroy'])->name('planos.destroy');
-Route::get('admin/planos/{url}', [PlanoController::class, 'show'])->name('planos.show');
-Route::post('admin/planos/salvar', [PlanoController::class, 'store'])->name('planos.store');
-Route::get('admin/planos', [PlanoController::class, 'index'])->name('planos.index');
 
-Route::get('admin', [PlanoController::class, 'index'])->name('admin.index');
+Route::prefix('admin')->group(function() {
+    Route::get('planos/novo', [PlanoController::class, 'create'])->name('planos.create');
+    Route::put('planos/{url}', [PlanoController::class, 'update'])->name('planos.update');
+    Route::get('planos/{url}/editar', [PlanoController::class, 'edit'])->name('planos.edit');
+    Route::any('planos/filtrar', [PlanoController::class, 'search'])->name('planos.search');
+    Route::delete('planos/{url}', [PlanoController::class, 'destroy'])->name('planos.destroy');
+    Route::get('planos/{url}', [PlanoController::class, 'show'])->name('planos.show');
+    Route::post('planos/salvar', [PlanoController::class, 'store'])->name('planos.store');
+    Route::get('planos', [PlanoController::class, 'index'])->name('planos.index');
+
+    Route::get('/', [PlanoController::class, 'index'])->name('admin.index');
+});
+
 
 Route::get('/', function () {
     return view('welcome');

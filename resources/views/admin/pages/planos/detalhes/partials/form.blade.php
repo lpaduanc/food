08b40214ead2e14e325ec9d0559fb18a77a1e8f5
Luiz Paduan <luiz.paduanit@gmail.com>
2021-05-18 23:54:34 -0300

    Cadastro de detalhe do plano

---
+++ resources/views/admin/pages/planos/detalhes/partials/form.blade.php
@@ -0,0 +1,9 @@
+@csrf
+
+<div class="form-group">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome" class="form-control">
+</div>
+<div class="form-group">
+    <button type="submit" class="btn btn-info">Salvar</button>
+</div>
